b8fd3c24bcdde20f2e6cd6ab022ed41f2d4825d2
guanjinyu <guanjinyu@itouchtv.cn>
2020-03-05 13:02:35 +0800

    add record script

---
+++ index.js
@@ -0,0 +1,53 @@
+const fs = require('fs-extra')
+const path = require('path')
+
+const readFiles = async (filePath, name, callback) => {
+    const files = fs.readdirSync(filePath)
+    files
+        .map(file => {
+            const filedir = path.join(filePath, file)
+            const stats = fs.statSync(filedir)
+            if (stats.isFile() && filedir.indexOf(name) > -1) {
+                callback && callback(filedir)
+            } else if (stats.isDirectory()) {
+                readFiles(filedir, name, callback)
+            }
+        })
+}
+
+const componentsPath = 'all-books'
+const bookPath = {}
+readFiles(
+    path.join(__dirname, './', componentsPath),
+    '.pdf',
+    (file, error) => {
+        if (error) {
+            return console.error('read files error:', error)
+        }
+        const _bookPath = file.split('/all-books/')[1].split('/')
+        if (!Reflect.has(bookPath, _bookPath[0])) {
+            bookPath[_bookPath[0]] = []
+        }
+        bookPath[_bookPath[0]].push(_bookPath[1])
+    }
+)
+
+let fileText = "# record books I've read\n"
+
+fileText += '\n' + '## catalogue\n\n'
+
+Object.keys(bookPath).map(key => {
+    fileText += `- ${key}\n`
+    const bookNames = bookPath[key].reduce((result, value) => {
+        return result += '  - ' + value + '\n'
+    }, '')
+    fileText += bookNames
+})
+
+console.log (fileText)
+
+fs.outputFileSync(
+    path.join(__dirname, './readme.md'),
+    fileText,
+    { encoding: 'utf-8' }
+)
--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
 # record books I've read
+
+## catalogue
+
+- front-end
+  - JavaScript高级程序设计（第3版）】中文 高清 完整 详细书签版.pdf
+  - SVG精髓（第2版）.pdf
+  - 《从零开始学微信小程序开发》.(高洪涛).[PDF]@ckook.pdf
+  - 一个前端的自我修养-winter.pdf
+  - 正念前端.pdf
+  - 深入React技术栈.pdf
+- linux
+  - 鸟哥的Linux私房菜-基础篇.第四版.pdf
